92564d77db23a6644e1ea30eee5d96b0a1504feb
na1vez <na1vez@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-07 15:30:24 +0000

    new version
    git-svn-id: https://plugins.svn.wordpress.org/uptolike-share/tags/1.2.7@1021579 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- usb-admin.php
+++ usb-admin.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 18.08.14
- * Time: 12:15
- */
 function constructorIframe($projectId, $partnerId, $mail, $cryptKey)
 {
 
@@ -16,8 +10,8 @@ function constructorIframe($projectId, $partnerId, $mail, $cryptKey)
     $signature = md5($paramsStr);
     $params['signature'] = $signature;
     if ('' !== $cryptKey) {
-        $finalUrl = 'http://uptolike.com/api/constructor.html?' . http_build_query($params);
-    } else $finalUrl = 'http://uptolike.com/api/constructor.html';
+        $finalUrl = 'https://uptolike.com/api/constructor.html?' . http_build_query($params);
+    } else $finalUrl = 'https://uptolike.com/api/constructor.html';
 
 
     return $finalUrl;
@@ -34,7 +28,7 @@ function statIframe($projectId, $partnerId, $mail, $cryptKey)
     $paramsStr = 'mail=' . $mail . '&partner=' . $partnerId . '&projectId=' . $projectId;
     $signature = md5($paramsStr . $cryptKey);
     $params['signature'] = $signature;
-    $finalUrl = 'http://uptolike.com/api/statistics.html?' . http_build_query($params);
+    $finalUrl = 'https://uptolike.com/api/statistics.html?' . http_build_query($params);
 
     return $finalUrl;
 }
@@ -46,7 +40,7 @@ function usb_admin_page()
 
     if ((isset($options['uptolike_email'])) && ('' !== $options['uptolike_email'])) {
         $email = $options['uptolike_email'];
-    } else $email = get_settings('admin_email');
+    } else $email = get_option('admin_email');
     $partnerId = 'cms';
     $projectId = 'cms' . preg_replace('/^www\./', '', $_SERVER['HTTP_HOST']);
     $projectId = str_replace('.', '', $projectId);
@@ -59,7 +53,7 @@ function usb_admin_page()
 
         if ((isset($this->options['uptolike_email'])) && ('' !== $this->options['uptolike_email'])) {
             $email = $this->options['uptolike_email'];
-        } else $email = get_settings('admin_email');
+        } else $email = get_option('admin_email');
         $partnerId = 'cms';
         $projectId = 'cms' . preg_replace('/^www\./', '', $_SERVER['HTTP_HOST']);l
         $projectId = str_replace('.','',$projectId);
@@ -71,7 +65,7 @@ function usb_admin_page()
     */
     ?>
     <script type="text/javascript">
-        <? include('main.js'); ?>
+        <?php include('main.js'); ?>
     </script>
     <style type="text/css">
         h2.placeholder {
@@ -123,16 +117,16 @@ function usb_admin_page()
                     </a>
                 </div>
                 <div class="wrapper-tab" id="con_stat">
-                    <? if (('' == $partnerId) OR ('' == $email) OR ('' == $cryptKey)) {
+                    <?php if (('' == $partnerId) OR ('' == $email) OR ('' == $cryptKey)) {
 
                         ?>
                         <h2>Статистика</h2>
                         <p>Для просмотра статистики необходимо ввести ваш секретный ключ </p>
-                    <? } else { ?>
+                    <?php } else { ?>
                         <!-- <?php print_r(array($partnerId, $email, $cryptKey)); ?> -->
                         <iframe style="width: 100%;height: 380px;" id="stats_iframe"
                                 data-src="<?php echo statIframe($projectId, $partnerId, $email, $cryptKey); ?>">
-                        </iframe> <?
+                        </iframe> <?php
                     } ?>
                     <button class="reg_btn" type="button">Запросить секретный ключ</button>
                     <br/>
@@ -175,7 +169,7 @@ function usb_admin_page()
                         <tr style="display:none">
                             <th scope="row">email для регистрации</th>
                             <td><input type="text" id="uptolike_email" name="my_option_name[uptolike_email]"
-                                       value="pk1z@ya3.ru">
+                                       value="">
                                 <button type="button" onclick="regMe();">Зарегистрироваться</button>
                             </td>
                         </tr>
@@ -184,6 +178,16 @@ function usb_admin_page()
                             <td><input type="checkbox" id="on_main" name="my_option_name[on_main]" checked="checked">
                             </td>
                         </tr>
+                        <tr>
+                            <th scope="row">Располагать блок на статических страницах</th>
+                            <td><input type="checkbox" id="on_page" name="my_option_name[on_page]" checked="checked">
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Убрать кнопки в анонсах постов</th>
+                            <td><input type="checkbox" id="on_page" name="my_option_name[on_page]" checked="checked">
+                            </td>
+                        </tr>                         
                         <tr>
                             <th scope="row">Расположение виджета</th>
                             <td><select id="widget_position" name="my_option_name[widget_position]">
@@ -204,6 +208,11 @@ function usb_admin_page()
                         </tbody>
                     </table>
                     <input type="submit" name="submit_btn" value="Cохранить изменения">
+                    
+                     <br>
+                         "Данный плагин полностью бесплатен. Мы регулярно его улучшаем и добавляем новые функции.<br>
+                         Пожалуйста, <a href="https://wordpress.org/support/view/plugin-reviews/uptolike-share">оставьте свой отзыв на данной странице</a>. Спасибо! <br>
+                       
                 </div>
 
 
