d7e66012daff84b6f6c1e0c4dca3e758b89fb922
na1vez <na1vez@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-17 10:00:33 +0000

    bugfix
    git-svn-id: https://plugins.svn.wordpress.org/uptolike-share/tags/1.3.2@1046855 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- usb-admin.php
+++ usb-admin.php
@@ -1,10 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 18.08.14
- * Time: 12:15
- */
+
+
+
 function constructorIframe($projectId, $partnerId, $mail, $cryptKey)
 {
 
@@ -16,8 +13,8 @@ function constructorIframe($projectId, $partnerId, $mail, $cryptKey)
     $signature = md5($paramsStr);
     $params['signature'] = $signature;
     if ('' !== $cryptKey) {
-        $finalUrl = 'http://uptolike.com/api/constructor.html?' . http_build_query($params);
-    } else $finalUrl = 'http://uptolike.com/api/constructor.html';
+        $finalUrl = 'https://uptolike.com/api/constructor.html?' . http_build_query($params);
+    } else $finalUrl = 'https://uptolike.com/api/constructor.html';
 
 
     return $finalUrl;
@@ -34,7 +31,7 @@ function statIframe($projectId, $partnerId, $mail, $cryptKey)
     $paramsStr = 'mail=' . $mail . '&partner=' . $partnerId . '&projectId=' . $projectId;
     $signature = md5($paramsStr . $cryptKey);
     $params['signature'] = $signature;
-    $finalUrl = 'http://uptolike.com/api/statistics.html?' . http_build_query($params);
+    $finalUrl = 'https://uptolike.com/api/statistics.html?' . http_build_query($params);
 
     return $finalUrl;
 }
@@ -46,7 +43,7 @@ function usb_admin_page()
 
     if ((isset($options['uptolike_email'])) && ('' !== $options['uptolike_email'])) {
         $email = $options['uptolike_email'];
-    } else $email = get_settings('admin_email');
+    } else $email = get_option('admin_email');
     $partnerId = 'cms';
     $projectId = 'cms' . preg_replace('/^www\./', '', $_SERVER['HTTP_HOST']);
     $projectId = str_replace('.', '', $projectId);
@@ -59,7 +56,7 @@ function usb_admin_page()
 
         if ((isset($this->options['uptolike_email'])) && ('' !== $this->options['uptolike_email'])) {
             $email = $this->options['uptolike_email'];
-        } else $email = get_settings('admin_email');
+        } else $email = get_option('admin_email');
         $partnerId = 'cms';
         $projectId = 'cms' . preg_replace('/^www\./', '', $_SERVER['HTTP_HOST']);l
         $projectId = str_replace('.','',$projectId);
@@ -71,7 +68,7 @@ function usb_admin_page()
     */
     ?>
     <script type="text/javascript">
-        <? include('main.js'); ?>
+        <?php include('main.js'); ?>
     </script>
     <style type="text/css">
         h2.placeholder {
@@ -123,16 +120,16 @@ function usb_admin_page()
                     </a>
                 </div>
                 <div class="wrapper-tab" id="con_stat">
-                    <? if (('' == $partnerId) OR ('' == $email) OR ('' == $cryptKey)) {
+                    <?php if (('' == $partnerId) OR ('' == $email) OR ('' == $cryptKey)) {
 
                         ?>
                         <h2>Статистика</h2>
                         <p>Для просмотра статистики необходимо ввести ваш секретный ключ </p>
-                    <? } else { ?>
+                    <?php } else { ?>
                         <!-- <?php print_r(array($partnerId, $email, $cryptKey)); ?> -->
                         <iframe style="width: 100%;height: 380px;" id="stats_iframe"
                                 data-src="<?php echo statIframe($projectId, $partnerId, $email, $cryptKey); ?>">
-                        </iframe> <?
+                        </iframe> <?php
                     } ?>
                     <button class="reg_btn" type="button">Запросить секретный ключ</button>
                     <br/>
@@ -154,56 +151,19 @@ function usb_admin_page()
                 </div>
 
                 <div class="wrapper-tab " id="con_settings">
-                    <input type="hidden" name="option_page" value="my_option_group"><input type="hidden" name="action"
-                                                                                           value="update"><input
-                        type="hidden" id="_wpnonce" name="_wpnonce" value="cac4f73a65"><input type="hidden"
-                                                                                              name="_wp_http_referer"
-                                                                                              value="/wp-admin/options-general.php?page=uptolike_settings">
-
-                    <h3>Настройки виджета</h3>
-                    <table class="form-table">
-                        <tbody>
-                        <tr style="display:none">
-                            <th scope="row">код виджета</th>
-                            <td><textarea id="widget_code" name="my_option_name[widget_code]"></textarea></td>
-                        </tr>
-                        <tr style="display:none">
-                            <th scope="row">Ключ(CryptKey)</th>
-                            <td><input type="text" class="id_number" name="my_option_name[id_number]" value=""
-                                       style="width: 520px;"></td>
-                        </tr>
-                        <tr style="display:none">
-                            <th scope="row">email для регистрации</th>
-                            <td><input type="text" id="uptolike_email" name="my_option_name[uptolike_email]"
-                                       value="pk1z@ya3.ru">
-                                <button type="button" onclick="regMe();">Зарегистрироваться</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Располагать блок на главной странице</th>
-                            <td><input type="checkbox" id="on_main" name="my_option_name[on_main]" checked="checked">
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Расположение виджета</th>
-                            <td><select id="widget_position" name="my_option_name[widget_position]">
-                                    <option value="top">Только сверху</option>
-                                    <option selected="selected" value="bottom">Только снизу</option>
-                                    <option value="both">Сверху и снизу</option>
-                                </select></td>
-                        </tr>
-                        <tr>
-                            <th scope="row">Обратная связь</th>
-                            <td><a href="mailto:support@uptolike.com" target="_top"> support@uptolike.com</a></td>
-                        </tr>
-                        <tr style="display:none">
-                            <th scope="row">настройки конструктора</th>
-                            <td><input type="hidden" id="uptolike_json" name="my_option_name[uptolike_json]" value="">
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+                         <?php
+                         $my_settings_page = new MySettingsPage();
+                         $my_settings_page->page_init();
+                        settings_fields('my_option_group');
+                        do_settings_sections($my_settings_page->settings_page_name);
+                        ?>
+                   
                     <input type="submit" name="submit_btn" value="Cохранить изменения">
+                    
+                     <br>
+                         "Данный плагин полностью бесплатен. Мы регулярно его улучшаем и добавляем новые функции.<br>
+                         Пожалуйста, <a href="https://wordpress.org/support/view/plugin-reviews/uptolike-share">оставьте свой отзыв на данной странице</a>. Спасибо! <br>
+                       
                 </div>
 
 
