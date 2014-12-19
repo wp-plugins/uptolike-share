7d16eb29901064ea11472ba7ed5beae4bd2f8ab6
na1vez <na1vez@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-19 12:45:32 +0000

    bugfix
    git-svn-id: https://plugins.svn.wordpress.org/uptolike-share/tags/1.3.4@1049228 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uptolike_share_widget.php
+++ uptolike_share_widget.php
@@ -1,9 +1,9 @@
-<?
+<?php
 /*
 Plugin Name: UpToLike Share Buttons
 Plugin URI: http://uptolike.ru/
 Description: Uptolike Share Buttons - social bookmarking widget with sharing statistics.
-Version: 1.2.2
+Version: 1.3.4
 Author URI: http://uptolike.ru/
 */
 
